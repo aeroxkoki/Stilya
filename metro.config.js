881e4c31bf33bddc3c57b43f82d4f6ae6b58a2d6
Koki <aeroxkoki@gmail.com>
2025-05-20 22:38:54 +0900

    fix: Improve build configuration for Expo 53 compatibility

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,9 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 const config = getDefaultConfig(__dirname);
+
+// パッケージエクスポートフィールド対応（問題が発生する場合のオプトアウト用）
+if (config.resolver) {
+  config.resolver.unstable_enablePackageExports = false;
+}
+
 module.exports = config;
