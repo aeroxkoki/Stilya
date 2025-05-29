5f80803d9394ba895f1c6ffaea502aa0be25b240
Koki Nishiwaki <aeroxkoki@gmail.com>
2025-05-29 16:21:42 +0900

    fix: Update Metro config for better network connectivity and add dev scripts to gitignore

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,22 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// カスタム設定を追加
+config.server = {
+  ...config.server,
+  port: 8081,
+  enhanceMiddleware: (middleware) => {
+    return (req, res, next) => {
+      // CORS headers for development
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      return middleware(req, res, next);
+    };
+  },
+};
+
+// Watchmanの設定を追加（オプション）
+config.watchFolders = [__dirname];
+
 module.exports = config;
